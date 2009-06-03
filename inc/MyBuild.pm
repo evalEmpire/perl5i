51c91845b4d3accbee4749e2ecb1a3aa3db58e58
Michael G. Schwern <schwern@pobox.com>
2009-06-03 13:27:01 -0700

    Fix the tidy action so it picks up .PL files.
    Rerun perltidy.

--- inc/MyBuild.pm
+++ inc/MyBuild.pm
@@ -10,14 +10,15 @@ sub ACTION_tidy {
     my %found_files = map { %$_ } $self->find_pm_files,
       $self->_find_file_by_type( 'pm', 't' ),
       $self->_find_file_by_type( 'pm', 'inc' ),
-      $self->_find_file_by_type( 't',  't' );
-    $self->_find_file_by_type( 'PL', '.' );
+      $self->_find_file_by_type( 't',  't' ),
+      $self->_find_file_by_type( 'PL', '.' );
 
     my @files = ( keys %found_files, map { $self->localize_file_path($_) } @extra );
 
-    print join "\n", @files;
 
-    for my $file (@files) {
+    print "Running perltidy on @{[ scalar @files ]} files...\n";
+    for my $file ( sort { $a cmp $b } @files ) {
+        print "  $file\n";
         system( 'perltidy', '-b', $file );
         unlink("$file.bak") if $? == 0;
     }
--- t/dump/code.t
+++ t/dump/code.t
@@ -6,17 +6,18 @@ use perl5i;
 use Test::More;
 
 sub code_dump_ok($$;$) {
-    my($code, $want, $name) = @_;
+    my( $code, $want, $name ) = @_;
 
-    my $ref = eval($code->perl);
+    my $ref = eval( $code->perl );
     ok( $ref, "dump eval'd" ) or do { diag $@; return; };
-    is_deeply [$ref->()], $want, $name;
+    is_deeply [ $ref->() ], $want, $name;
 }
 
 
 # Test closures
 {
     my $foo = 42;
+
     sub closure {
         return $foo;
     }
@@ -25,7 +26,7 @@ sub code_dump_ok($$;$) {
 my $closure = \&closure;
 TODO: {
     local $TODO = "closures aren't dumped properly";
-    code_dump_ok $closure,   [42], "scalar code ref dump";
+    code_dump_ok $closure, [42], "scalar code ref dump";
 }
 
 code_dump_ok sub { 23 }, [23], "anon sub dump";
