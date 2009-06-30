3d963733aa54da08d42dd6676411434063fcd522
Darian Anthony Patrick <dap@darianpatrick.com>
2009-06-30 18:55:58 -0400

    Integrated compilation of perl5i.c into build/install process

--- inc/MyBuild.pm
+++ inc/MyBuild.pm
@@ -2,6 +2,30 @@ package MyBuild;
 
 use base 'Module::Build';
 
+# Override default 'build' action
+# to allow compilation of perl5i.c
+sub ACTION_build {
+    my $self = shift;
+
+    my ($obj_file, $exe_file);
+    eval {
+        my $b = $self->cbuilder();
+        $obj_file = $b->compile(
+            source               => 'bin/perl5i.c',
+            extra_compiler_flags => '-std=c99'
+        );
+        $exe_file = $b->link_executable(objects => $obj_file);
+    };
+    if ($@) {
+        print STDERR "Error: No C compiler available; please install ExtUtils::CBuilder\n";
+    }
+
+    $self->add_to_cleanup($obj_file, $exe_file);
+
+    # Invoke parent 'build' action
+    $self->SUPER::ACTION_build();
+}
+
 # Run perltidy over all the Perl code
 # Borrowed from Test::Harness
 sub ACTION_tidy {
