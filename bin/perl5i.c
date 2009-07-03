8b8bab91fa04537485132c53ecc565de8f36e37f
Michael G. Schwern <schwern@pobox.com>
2009-07-02 21:31:01 -0700

    Fix up perl5i.c to do an exec and avoid the shell.

--- bin/perl5i.c
+++ bin/perl5i.c
@@ -1,32 +1,21 @@
- 
-#include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main (int argc, char** argv) {
+/*
+ * Meant to mimic the shell command
+ *     exec perl -Mperl5i "$@"
+ */
 
-    /*
-     * Meant to mimic the following shell command
-     *     exec %s -Mperl5i "$@"
-     */
-    char* perlcmd = "perl -Mperl5i";
-    char* cmd = (char *) malloc( strlen(perlcmd) + 1);
-    strcpy( cmd, perlcmd );
+int main (int argc, char* argv[]) {
+    int i;
+    char* perl_cmd = "perl";
+    char** perl_args = malloc(sizeof(char*) * (argc + 1));
+    perl_args[0] = "perl5i";
+    perl_args[1] = "-Mperl5i";
 
-    for ( int i=1; i < argc; i++ ) {
-        /*
-         * Extend cmd length adding:
-         *     one byte for trailing null,
-         *     one byte for pre-pended space,
-         *     two bytes for quotes
-         */
-        cmd = realloc( cmd, strlen(cmd) + strlen(argv[i]) + 4 );
-        strcat( cmd, " \""   );
-        strcat( cmd, argv[i] );
-        strcat( cmd, "\""    );
+    for( i = 1;  i < argc;  i++ ) {
+        perl_args[i+1] = argv[i];
     }
 
-    int ret = system( cmd );
-    return( ret );
+    return execvp( perl_cmd, perl_args );
 }
-
