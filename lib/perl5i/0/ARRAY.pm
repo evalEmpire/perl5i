5de77c92df4c4734a90402eae07510b8c343f14a
Bruno Vecchi <vecchi@gmail.com>
2010-02-17 23:30:22 -0300

    Implemented autoboxed List::MoreUtils functions: all, any, none, minmax, true, false, uniq and zip"

--- lib/perl5i/0/ARRAY.pm
+++ lib/perl5i/0/ARRAY.pm
@@ -4,7 +4,7 @@ use 5.010;
 
 use strict;
 use warnings;
-use autobox;
+use Module::Load;
 
 sub ARRAY::grep {
     my ( $array, $filter ) = @_;
@@ -15,5 +15,50 @@ sub ARRAY::grep {
 
     return [ CORE::grep { $filter->($_) } @$array ];
 }
+
+sub ARRAY::all {
+    load List::MoreUtils;
+    return List::MoreUtils::all($_[1], @{$_[0]});
+}
+
+sub ARRAY::any {
+    load List::MoreUtils;
+    return List::MoreUtils::any($_[1], @{$_[0]});
+}
+
+sub ARRAY::none {
+    load List::MoreUtils;
+    return List::MoreUtils::none($_[1], @{$_[0]});
+}
+
+sub ARRAY::true {
+    load List::MoreUtils;
+    return List::MoreUtils::true($_[1], @{$_[0]});
+}
+
+sub ARRAY::false {
+    load List::MoreUtils;
+    return List::MoreUtils::false($_[1], @{$_[0]});
+
+}
+
+sub ARRAY::uniq {
+    load List::MoreUtils;
+    return [ List::MoreUtils::uniq(@{$_[0]}) ];
+}
+
+sub ARRAY::minmax {
+    load List::MoreUtils;
+    return [ List::MoreUtils::minmax(@{$_[0]}) ];
+}
+
+*ARRAY::range = *ARRAY::minmax;
+
+sub ARRAY::zip {
+    load List::MoreUtils;
+    return [ List::MoreUtils::zip(@_) ];
+}
+
+*ARRAY::mesh = *ARRAY::zip;
 
 1;
