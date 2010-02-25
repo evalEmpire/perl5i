a0177a2a71fa6d4f298c578b52d8419ed86da7a2
Bruno Vecchi <vecchi.b@gmail.com>
2010-02-25 17:06:37 -0300

    implemented ARRAY::intersect()

--- lib/perl5i/0/ARRAY.pm
+++ lib/perl5i/0/ARRAY.pm
@@ -116,6 +116,44 @@ sub _diff_two {
     return $diff;
 }
 
+sub ARRAY::intersect {
+    my ($base, @rest) = @_;
+    return $base unless (@rest);
+
+    # XXX If I use carp here, the exception is "bizarre copy of ARRAY in
+    # ssasign ... "
+    die "Arguments must be array references" if grep { ref $_ ne 'ARRAY' } @rest;
+
+    foreach my $array (@rest) {
+        $base = _intersect_two($base, $array);
+    }
+
+    return $base;
+}
+
+sub _intersect_two {
+    # Compare differences between two arrays.
+    my ($c, $d) = @_;
+
+    my $intersect = [];
+
+    # For each element of $c, try to find if it is equal to any of the
+    # elements of $d. If not, it's unique, and has to be pushed into
+    # $diff.
+
+    require List::MoreUtils;
+    foreach my $item (@$c) {
+        if (
+            List::MoreUtils::any( sub { _are_equal( $item, $_ ) }, @$d )
+        )
+        {
+            push @$intersect, $item;
+        }
+    }
+
+    return $intersect;
+}
+
 sub _are_equal {
     my ($r1, $r2) = @_;
 
