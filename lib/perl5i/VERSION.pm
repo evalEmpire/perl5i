96131ee26207d92bc41604e2299cffd9cc34d242
Michael G. Schwern <schwern@pobox.com>
2010-11-24 16:37:10 +1100

    Version 2.4.1

--- lib/perl5i/VERSION.pm
+++ lib/perl5i/VERSION.pm
@@ -5,7 +5,7 @@ package perl5i::VERSION;
 use strict;
 use warnings;
 
-use version 0.77; our $VERSION = qv("v2.4.0");
+use version 0.77; our $VERSION = qv("v2.4.1");
 
 sub latest { "perl5i::2" };     # LATEST HERE (for automated update)
 
