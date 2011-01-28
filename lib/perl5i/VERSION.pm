8a186f2d1328080ff95776fa258bcf2304ecad0a
Michael G. Schwern <schwern@pobox.com>
2011-01-28 11:04:39 +1000

    v2.5.1
    This was a manual release to manually fix the META.yml file so "true" is not interpreted as
    1.  [github 173]

--- lib/perl5i/VERSION.pm
+++ lib/perl5i/VERSION.pm
@@ -5,7 +5,7 @@ package perl5i::VERSION;
 use strict;
 use warnings;
 
-use version 0.77; our $VERSION = qv("v2.5.0");
+use version 0.77; our $VERSION = qv("v2.5.1");
 
 sub latest { "perl5i::2" };     # LATEST HERE (for automated update)
 
